f5abf2725e9456ea61bb7d028520744e2cf991f5
nowbrother <nomagold57@gmail.com>
2024-09-07 21:29:24 +0900

    project rename2

---
+++ planTree/src/main/java/com/knowledge_expedition/planTree/PlanTreeApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge_expedition.planTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanTreeApplication.class, args);
+	}
+
+}
