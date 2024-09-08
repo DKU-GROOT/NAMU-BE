b93a6e6ab454aed871f4be2d114e0f0cc4e0fd11
nowbrother <nomagold57@gmail.com>
2024-09-08 15:29:29 +0900

    create Entity, Repository

---
+++ planTree/src/main/java/com/knowledge_expedition/planTree/repository/CertificationRepository.java
@@ -0,0 +1,11 @@
+package com.knowledge_expedition.planTree.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.knowledge_expedition.planTree.entity.CertificationEntity;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<CertificationEntity, String> {
+    
+}
