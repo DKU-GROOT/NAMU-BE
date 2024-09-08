bed71dc2db33e85323adf52afd09fb89169adbe5
nowbrother <nomagold57@gmail.com>
2024-09-08 15:29:29 +0900

    create Entity, Repository

---
+++ planTree/src/main/java/com/knowledge_expedition/planTree/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.knowledge_expedition.planTree.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.knowledge_expedition.planTree.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    
+}
