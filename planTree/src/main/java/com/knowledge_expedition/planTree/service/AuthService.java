99dc7204a81e16d3dd585f58f925b46e6149e8da
now-brother <nomagold57@gmail.com>
2024-09-20 15:07:04 +0900

    ADD Log-in API

--- planTree/src/main/java/com/knowledge_expedition/planTree/service/AuthService.java
+++ planTree/src/main/java/com/knowledge_expedition/planTree/service/AuthService.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import com.knowledge_expedition.planTree.dto.request.auth.CheckCertificationRequestDto;
 import com.knowledge_expedition.planTree.dto.request.auth.EmailCertificationRequestDto;
 import com.knowledge_expedition.planTree.dto.request.auth.IdCheckRequestDto;
+import com.knowledge_expedition.planTree.dto.request.auth.SignInRequestDto;
 import com.knowledge_expedition.planTree.dto.request.auth.SignUpRequestDto;
 import com.knowledge_expedition.planTree.dto.response.auth.CheckCertificationResponseDto;
 import com.knowledge_expedition.planTree.dto.response.auth.EmailCertificationResponseDto;
 import com.knowledge_expedition.planTree.dto.response.auth.IdCheckResponseDto;
+import com.knowledge_expedition.planTree.dto.response.auth.SignInResponseDto;
 import com.knowledge_expedition.planTree.dto.response.auth.SignUpResponseDto;
 
 public interface AuthService {
@@ -17,4 +19,5 @@ public interface AuthService {
     ResponseEntity<? super EmailCertificationResponseDto> emailCertification(EmailCertificationRequestDto dto);
     ResponseEntity<? super CheckCertificationResponseDto> checkCertification(CheckCertificationRequestDto dto);
     ResponseEntity<? super SignUpResponseDto> signUp (SignUpRequestDto dto);
+    ResponseEntity<? super SignInResponseDto> signIn (SignInRequestDto dto);
 }
