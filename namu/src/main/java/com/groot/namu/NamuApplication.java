26c658134f21761830a990dd9ec3d4e06b4d2479
now-brother <nomagold57@gmail.com>
2024-10-24 15:26:05 +0900

    EDIT:2024-10-24

--- namu/src/main/java/com/groot/namu/NamuApplication.java
+++ namu/src/main/java/com/groot/namu/NamuApplication.java
@@ -9,8 +9,4 @@ public class NamuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NamuApplication.class, args);
 	}
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 }
