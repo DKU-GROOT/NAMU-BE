2be510f2c58882d66dbc54c5e7793c90aac0c7e9
now-brother <nomagold57@gmail.com>
2024-10-24 15:35:36 +0900

    Delete All

--- namu/src/main/java/com/groot/namu/dto/response/study/SummaryResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.groot.namu.dto.response.study;
-
-import com.groot.namu.dto.response.ResponseDto;
-
-public class SummaryResponseDto extends ResponseDto{
-    private SummaryResponseDto() {
-        super();
-    }
-}
