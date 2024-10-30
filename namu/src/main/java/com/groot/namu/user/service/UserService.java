55087ec6b6044559b606238e310388317b8b319c
Lee Jae Hyung <nomagold57@gmail.com>
2024-10-30 23:43:54 +0900

    Remove OAuth2.0

--- namu/src/main/java/com/groot/namu/user/service/UserService.java
+++ namu/src/main/java/com/groot/namu/user/service/UserService.java
@@ -19,33 +19,5 @@ public class UserService {
     private final UserRepository userRepository;
     private final TreeRepository treeRepository;
 
-    @Transactional
-    public User registerUser(String email, String nickname) {
-        User user = new User(email, nickname);
-        userRepository.save(user);
-        
-        Tree tree = new Tree(user);
-        treeRepository.save(tree);
-
-        return user;
-    }
-
-    public boolean isEmailExists(String email) {
-        return userRepository.findByEmail(email).isPresent();
-    }
-
-    public boolean login(OAuth2User oAuth2User) {
-        // OAuth2User에서 이메일 정보 추출
-        String email = oAuth2User.getAttribute("email");
-
-        if (email == null) {
-            throw new IllegalArgumentException("Email not found in OAuth2User attributes.");
-        }
-
-        // 데이터베이스에서 이메일로 사용자 검색
-        Optional<User> userOptional = userRepository.findByEmail(email);
-
-        // 사용자가 존재하는지 확인하여 로그인 처리
-        return userOptional.isPresent();
-    }
+    
 }
