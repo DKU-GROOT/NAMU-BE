0f99b7a0b6629a4cf50e5d8bd179bd30f16bf035
Lee Jae Hyung <nomagold57@gmail.com>
2024-10-31 11:40:27 +0900

    tree entity

--- namu/src/main/java/com/groot/namu/tree/entity/TreeEntity.java
+++ namu/src/main/java/com/groot/namu/tree/entity/TreeEntity.java
@@ -1,6 +1,8 @@
 package com.groot.namu.tree.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Entity(name="tree")
