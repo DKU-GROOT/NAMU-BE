a9199a4068ebabc3a4b3f197cfaed28eaf5ed631
Lee Jae Hyung <nomagold57@gmail.com>
2024-10-31 11:07:28 +0900

    tree repository

--- namu/src/main/java/com/groot/namu/tree/repository/TreeRepository.java
+++ namu/src/main/java/com/groot/namu/tree/repository/TreeRepository.java
@@ -2,8 +2,9 @@ package com.groot.namu.tree.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.groot.namu.tree.entity.Tree;
+import com.groot.namu.tree.entity.TreeEntity;
 
-public interface TreeRepository extends JpaRepository<Tree, String>{
-    
+public interface TreeRepository extends JpaRepository<TreeEntity, String>{
+    boolean existsByEmail(String email);
+    TreeEntity findByEmail(String email);
 }
