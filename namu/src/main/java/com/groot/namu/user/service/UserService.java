5a4adf334cd3149bfdf6aed04958a3069fae7e22
Lee Jae Hyung <nomagold57@gmail.com>
2024-10-31 11:27:57 +0900

    user service interface

--- namu/src/main/java/com/groot/namu/user/service/UserService.java
+++ namu/src/main/java/com/groot/namu/user/service/UserService.java
@@ -5,10 +5,15 @@ import org.springframework.http.ResponseEntity;
 import com.groot.namu.user.dto.request.CheckCertificationRequestDto;
 import com.groot.namu.user.dto.request.EmailCertificationRequestDto;
 import com.groot.namu.user.dto.request.EmailCheckRequestDto;
+import com.groot.namu.user.dto.request.NicknameCheckRequestDto;
 import com.groot.namu.user.dto.request.SignInRequestDto;
 import com.groot.namu.user.dto.request.SignUpRequestDto;
 import com.groot.namu.user.dto.response.CheckCertificationResponseDto;
 import com.groot.namu.user.dto.response.SignInResponseDto;
+import com.groot.namu.user.dto.response.NicknameCheckResponseDto;
+import com.groot.namu.user.dto.response.EmailCheckResponseDto;
+import com.groot.namu.user.dto.response.EmailCertificationResponseDto;
+import com.groot.namu.user.dto.response.SignUpResponseDto;
 
 public interface UserService {
     ResponseEntity<? super EmailCheckResponseDto> emailCheck(EmailCheckRequestDto dto);
