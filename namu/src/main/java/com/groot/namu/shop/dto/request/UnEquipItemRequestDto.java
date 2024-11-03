ae4763a9f08d152ffc907449d5fd0b9434b86ce0
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-03 14:47:39 +0900

    unequip item request dto

---
+++ namu/src/main/java/com/groot/namu/shop/dto/request/UnEquipItemRequestDto.java
@@ -0,0 +1,18 @@
+package com.groot.namu.shop.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnEquipItemRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String itemName;
+}
