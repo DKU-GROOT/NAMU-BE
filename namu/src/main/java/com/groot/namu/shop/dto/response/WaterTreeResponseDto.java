ea5a5281178f81423f85f17c90531dc1186b0d82
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-03 18:41:35 +0900

    water tree response dto

--- namu/src/main/java/com/groot/namu/shop/dto/response/WaterTreeResponseDto.java
+++ namu/src/main/java/com/groot/namu/shop/dto/response/WaterTreeResponseDto.java
@@ -3,6 +3,8 @@ package com.groot.namu.shop.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.groot.namu.global.common.ResponseCode;
+import com.groot.namu.global.common.ResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -16,5 +18,10 @@ public class WaterTreeResponseDto extends ResponseDto {
     public static ResponseEntity<WaterTreeResponseDto> success() {
         WaterTreeResponseDto responseBody = new WaterTreeResponseDto();
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> entityNotFound () {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.ENTITY_NOT_FOND, ResponseMessage.ENTITY_NOT_FOND);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
