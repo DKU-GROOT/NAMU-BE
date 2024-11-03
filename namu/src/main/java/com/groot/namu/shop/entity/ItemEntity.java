db8a2373ce5c26a66beaf0dc1d11056e4309178d
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-03 21:01:52 +0900

    item entity

--- namu/src/main/java/com/groot/namu/shop/entity/ItemEntity.java
+++ namu/src/main/java/com/groot/namu/shop/entity/ItemEntity.java
@@ -1,21 +1,28 @@
 package com.groot.namu.shop.entity;
 
+import com.groot.namu.shop.dto.request.AddItemListRequestDto;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity(name="item")
 @Table(name="item")
 public class ItemEntity {
     @Id
-    @GeneratedValue
-    private int itemId;
     private String itemName;
     private int price;
     private String type;
+
+    public ItemEntity(AddItemListRequestDto dto){
+        this.itemName = dto.getItemName();
+        this.price = dto.getPrice();
+        this.type = dto.getType();
+    }
 }
