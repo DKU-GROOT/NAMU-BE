d9b56483516bcedd175e5874f173ce039b095a06
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 03:16:01 +0900

    delete check list request dto

---
+++ namu/src/main/java/com/groot/namu/checkList/dto/request/DeleteCheckListRequestDto.java
@@ -0,0 +1,21 @@
+package com.groot.namu.checkList.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DeleteCheckListRequestDto {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String date;
+    @NotNull
+    @Min(value = 1)
+    private int num;
+}
