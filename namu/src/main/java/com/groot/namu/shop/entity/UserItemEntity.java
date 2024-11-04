dae7543a4b05b6d42d1b650066ac442123d42aae
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-04 17:12:02 +0900

    user item entity

--- namu/src/main/java/com/groot/namu/shop/entity/UserItemEntity.java
+++ namu/src/main/java/com/groot/namu/shop/entity/UserItemEntity.java
@@ -2,6 +2,7 @@ package com.groot.namu.shop.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -15,8 +16,8 @@ import lombok.Setter;
 @Table(name="user_item")
 public class UserItemEntity {
     @Id
-    @GeneratedValue
-    private int userItemId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userItemId;
     private String email;
     private String itemName;
     private boolean equip;
