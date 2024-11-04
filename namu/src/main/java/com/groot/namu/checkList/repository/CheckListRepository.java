95df83650f8a3eee7b798a35117d93b7e9aa9aa1
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-04 20:40:55 +0900

    check list repository

--- namu/src/main/java/com/groot/namu/checkList/repository/CheckListRepository.java
+++ namu/src/main/java/com/groot/namu/checkList/repository/CheckListRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.groot.namu.checkList.entity.CheckListEntity;
 
 public interface CheckListRepository extends JpaRepository<CheckListEntity, Long>{
-    CheckListEntity findByEmailandDate(String email, String date);
-    CheckListEntity existsByEmailandDate(String email, String date);
+    CheckListEntity findByEmailAndDate(String email, String date);
+    CheckListEntity existsByEmailAndDate(String email, String date);
 }
