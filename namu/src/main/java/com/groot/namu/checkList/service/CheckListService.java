e5cea6b538db8304250ea560d6fffbc9b147244e
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 00:29:09 +0900

    check list service

--- namu/src/main/java/com/groot/namu/checkList/service/CheckListService.java
+++ namu/src/main/java/com/groot/namu/checkList/service/CheckListService.java
@@ -2,9 +2,10 @@ package com.groot.namu.checkList.service;
 
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.checkList.dto.request.AddCheckListRequestDto;
-import com.groot.namu.checkList.dto.response.AddCheckListResponseDto;
+import com.groot.namu.checkList.dto.request.*;
+import com.groot.namu.checkList.dto.response.*;
 
 public interface CheckListService {
     ResponseEntity<? super AddCheckListResponseDto> addCheckList(AddCheckListRequestDto dto);
+    ResponseEntity<? super CompleteCheckListResponseDto> completeCheckList(CompleteCheckListRequestDto dto);
 }
