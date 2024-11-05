8d71eef900af4c0ef4a42591d2f409c1118500ae
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 21:46:41 +0900

    global response message

--- namu/src/main/java/com/groot/namu/global/common/ResponseMessage.java
+++ namu/src/main/java/com/groot/namu/global/common/ResponseMessage.java
@@ -2,21 +2,6 @@ package com.groot.namu.global.common;
 
 public interface ResponseMessage {
     String SUCCESS = "success";
-
-    String VALIDATION_FAIL = "validation failed";
-    String DUPLICATE_EMAIL = "duplicated email";
-    String DUPLICATE_ID = "duplicate id";
-    String ENTITY_NOT_FOND = "entity not found";
-    
-    String SIGN_IN_FAIL = "login informaion mismatch";
-    String CERTIFRCATION_FAIL = "certification failed";
-    String QUESTION_FAIL = "question failed";
-
-    String MAIL_FAIL = "Mail send failed";
     String DATABASE_ERROR = "database error";
-
-    String USER_NOT_EXISTS = "user is not exists";
-
-    String SUMMARY_FAIL = "making summary failed";
-    String EXAM_FAIL = "making exam failed";
+    String VALIDATION_FAIL = "validation failed";
 }
