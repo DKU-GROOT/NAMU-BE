8f4459f6e082ad1987a90b9a1c007a544f7eb43f
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 21:46:54 +0900

    study response code

---
+++ namu/src/main/java/com/groot/namu/study/common/StudyResponseCode.java
@@ -0,0 +1,9 @@
+package com.groot.namu.study.common;
+
+import com.groot.namu.global.common.ResponseCode;
+
+public interface StudyResponseCode extends ResponseCode{
+    String SUMMARY_FAIL = "SF";
+    String EXAM_FAIL = "EF";
+    String QUESTION_FAIL = "QF";
+}
