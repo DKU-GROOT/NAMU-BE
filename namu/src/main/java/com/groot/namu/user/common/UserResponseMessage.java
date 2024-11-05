646a6d3802450fb92d25b2d670f7204bdbc31ae3
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 21:47:47 +0900

    user response message

---
+++ namu/src/main/java/com/groot/namu/user/common/UserResponseMessage.java
@@ -0,0 +1,11 @@
+package com.groot.namu.user.common;
+
+public class UserResponseMessage {
+    String MAIL_FAIL = "Mail send failed";
+    String USER_NOT_EXISTS = "user is not exists";
+    String DUPLICATE_EMAIL = "duplicated email";
+    String DUPLICATE_ID = "duplicate id";
+    String ENTITY_NOT_FOND = "entity not found";
+    String SIGN_IN_FAIL = "login informaion mismatch";
+    String CERTIFRCATION_FAIL = "certification failed";
+}
