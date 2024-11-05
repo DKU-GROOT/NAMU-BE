2f88db7336d9fa428b2a3ce5cfceb7f11bbd95b8
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:10:04 +0900

    sign up response dto

--- namu/src/main/java/com/groot/namu/user/dto/response/SignUpResponseDto.java
+++ namu/src/main/java/com/groot/namu/user/dto/response/SignUpResponseDto.java
@@ -3,8 +3,8 @@ package com.groot.namu.user.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
+import com.groot.namu.user.common.UserResponseCode;
+import com.groot.namu.user.common.UserResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -21,12 +21,12 @@ public class SignUpResponseDto extends ResponseDto{
     }
 
     public static ResponseEntity<ResponseDto> duplicateId () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.DUPLICATE_ID, ResponseMessage.DUPLICATE_ID);
+        ResponseDto responseBody = new ResponseDto(UserResponseCode.DUPLICATE_ID, UserResponseMessage.DUPLICATE_ID);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
     public static ResponseEntity<ResponseDto> certificationFail () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFRCATION_FAIL, ResponseMessage.CERTIFRCATION_FAIL);
+        ResponseDto responseBody = new ResponseDto(UserResponseCode.CERTIFICATION_FAIL, UserResponseMessage.CERTIFICATION_FAIL);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
