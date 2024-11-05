f07634c6221ab215219592af8c66c69fa64228c5
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:09:18 +0900

    email check response dto

--- namu/src/main/java/com/groot/namu/user/dto/response/EmailCheckResponseDto.java
+++ namu/src/main/java/com/groot/namu/user/dto/response/EmailCheckResponseDto.java
@@ -3,8 +3,8 @@ package com.groot.namu.user.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
+import com.groot.namu.user.common.UserResponseCode;
+import com.groot.namu.user.common.UserResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -21,7 +21,7 @@ public class EmailCheckResponseDto extends ResponseDto{
     }
 
     public static ResponseEntity<ResponseDto> duplicatedId() {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.DUPLICATE_ID, ResponseMessage.DUPLICATE_ID);
+        ResponseDto responseBody = new ResponseDto(UserResponseCode.DUPLICATE_ID, UserResponseMessage.DUPLICATE_ID);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 }
