014b16b4ba04eb966f499e698314f60ae4f478f3
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:08:42 +0900

    check certification response dto

--- namu/src/main/java/com/groot/namu/user/dto/response/CheckCertificationResponseDto.java
+++ namu/src/main/java/com/groot/namu/user/dto/response/CheckCertificationResponseDto.java
@@ -3,8 +3,8 @@ package com.groot.namu.user.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
+import com.groot.namu.user.common.UserResponseCode;
+import com.groot.namu.user.common.UserResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -21,7 +21,7 @@ public class CheckCertificationResponseDto extends ResponseDto{
     }
 
     public static ResponseEntity<ResponseDto> certificationFail () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFRCATION_FAIL, ResponseMessage.CERTIFRCATION_FAIL);
+        ResponseDto responseBody = new ResponseDto(UserResponseCode.CERTIFICATION_FAIL, UserResponseMessage.CERTIFICATION_FAIL);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
