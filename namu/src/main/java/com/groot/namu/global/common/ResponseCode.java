065727cebdd3e58a156f7841256c58bd7599c3fa
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 21:46:28 +0900

    global response code

--- namu/src/main/java/com/groot/namu/global/common/ResponseCode.java
+++ namu/src/main/java/com/groot/namu/global/common/ResponseCode.java
@@ -2,22 +2,6 @@ package com.groot.namu.global.common;
 
 public interface ResponseCode {
     String SUCCESS = "SU";
-
-    String VALIDATION_FAIL = "VF";
-    String DUPLICATE_EMAIL = "DE";
-    String DUPLICATE_ID = "DI";
-    
-    String SIGN_IN_FAIL = "SF";
-    String CERTIFRCATION_FAIL = "CF";
-    String QUESTION_FAIL = "QF";
-
-    String MAIL_FAIL = "MF";
     String DATABASE_ERROR = "DBE";
-
-    String USER_NOT_EXISTS = "UNE";
-
-    String ENTITY_NOT_FOND = "ENF";
-
-    String SUMMARY_FAIL = "SF";
-    String EXAM_FAIL = "EF";
+    String VALIDATION_FAIL = "VF";
 }
