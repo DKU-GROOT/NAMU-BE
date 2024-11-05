255e5433c21ab52a3933d626cfbafd8eeeeb37af
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:06:37 +0900

    chat response dto

--- namu/src/main/java/com/groot/namu/study/dto/response/ChatResponseDto.java
+++ namu/src/main/java/com/groot/namu/study/dto/response/ChatResponseDto.java
@@ -2,8 +2,8 @@ package com.groot.namu.study.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
+import com.groot.namu.study.common.StudyResponseCode;
+import com.groot.namu.study.common.StudyResponseMessage;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -49,7 +49,7 @@ public class ChatResponseDto extends ResponseDto {
     }
 
     public static ResponseEntity<ResponseDto> questionFail() {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.QUESTION_FAIL, ResponseMessage.QUESTION_FAIL);
+        ResponseDto responseBody = new ResponseDto(StudyResponseCode.QUESTION_FAIL, StudyResponseMessage.QUESTION_FAIL);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
