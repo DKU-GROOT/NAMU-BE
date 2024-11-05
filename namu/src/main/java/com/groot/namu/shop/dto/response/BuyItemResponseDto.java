be9a6c7904329a5181a34f76cb2ca788f0c2ba2d
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:05:35 +0900

    buy item repsonse dto

--- namu/src/main/java/com/groot/namu/shop/dto/response/BuyItemResponseDto.java
+++ namu/src/main/java/com/groot/namu/shop/dto/response/BuyItemResponseDto.java
@@ -3,8 +3,6 @@ package com.groot.namu.shop.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -18,15 +16,5 @@ public class BuyItemResponseDto extends ResponseDto{
     public static ResponseEntity<BuyItemResponseDto> success() {
         BuyItemResponseDto responseBody = new BuyItemResponseDto();
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
-    }
-
-    public static ResponseEntity<ResponseDto> duplicateId () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.DUPLICATE_ID, ResponseMessage.DUPLICATE_ID);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
-    }
-
-    public static ResponseEntity<ResponseDto> certificationFail () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFRCATION_FAIL, ResponseMessage.CERTIFRCATION_FAIL);
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
