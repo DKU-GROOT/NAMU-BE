22617277a604d7406fe86be7afe6aea007d6d5da
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:05:21 +0900

    add item list response dto

--- namu/src/main/java/com/groot/namu/shop/dto/response/AddItemListResponseDto.java
+++ namu/src/main/java/com/groot/namu/shop/dto/response/AddItemListResponseDto.java
@@ -3,8 +3,6 @@ package com.groot.namu.shop.dto.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.groot.namu.global.common.ResponseCode;
-import com.groot.namu.global.common.ResponseMessage;
 import com.groot.namu.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -18,10 +16,5 @@ public class AddItemListResponseDto extends ResponseDto {
     public static ResponseEntity<AddItemListResponseDto> success() {
         AddItemListResponseDto responseBody = new AddItemListResponseDto();
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
-    }
-
-    public static ResponseEntity<ResponseDto> certificationFail () {
-        ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFRCATION_FAIL, ResponseMessage.CERTIFRCATION_FAIL);
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
