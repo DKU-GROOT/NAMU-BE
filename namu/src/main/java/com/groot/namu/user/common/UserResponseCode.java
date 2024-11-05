64b4daba37d7c6f559da330f0063aa20d29f321d
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-05 22:08:13 +0900

    user response code

--- namu/src/main/java/com/groot/namu/user/common/UserResponseCode.java
+++ namu/src/main/java/com/groot/namu/user/common/UserResponseCode.java
@@ -6,7 +6,7 @@ public interface UserResponseCode extends ResponseCode{
     String MAIL_FAIL = "MF";
     String USER_NOT_EXISTS = "UNE";
     String ENTITY_NOT_FOND = "ENF";
-    String CERTIFRCATION_FAIL = "CF";
+    String CERTIFICATION_FAIL = "CF";
     String DUPLICATE_EMAIL = "DE";
     String DUPLICATE_ID = "DI";
     String SIGN_IN_FAIL = "SF";
