cfaf00ce903f57033a3bdb0e7552e20b1aa5053c
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-06 21:02:50 +0900

    discussion entity

--- namu/src/main/java/com/groot/namu/discussion/entity/DiscussionEntity.java
+++ namu/src/main/java/com/groot/namu/discussion/entity/DiscussionEntity.java
@@ -1,5 +1,27 @@
 package com.groot.namu.discussion.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "discussion")
+@Table(name = "discussion")
 public class DiscussionEntity {
-    
+    @Id
+    private Long discussionId;
+    private String email;
+    private String title;
+    private String text;
+    private String subjectName;
+    private String postDate;
+    private String modifiedDate;
+    private boolean anonymous;
 }
