aba90c8889bdd4287e0b05c251da970a3c4575c5
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-07 15:10:37 +0900

    discussion controller

---
+++ namu/src/main/java/com/groot/namu/discussion/controller/DiscussionComtroller.java
@@ -0,0 +1,13 @@
+package com.groot.namu.discussion.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/namu/v2/discussion")
+public class DiscussionComtroller {
+    
+}
