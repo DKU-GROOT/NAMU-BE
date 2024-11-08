02c20a9c1348e4bf7074744ca78951a38b6e6467
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-08 16:58:15 +0900

    post discussion response dto

---
+++ namu/src/main/java/com/groot/namu/discussion/dto/response/PostDiscussionResponseDto.java
@@ -0,0 +1,17 @@
+package com.groot.namu.discussion.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.groot.namu.global.dto.ResponseDto;
+
+public class PostDiscussionResponseDto extends ResponseDto{
+    private PostDiscussionResponseDto() {
+        super();
+    }
+
+    public ResponseEntity<PostDiscussionResponseDto> success() {
+        PostDiscussionResponseDto responseBody = new PostDiscussionResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+}
