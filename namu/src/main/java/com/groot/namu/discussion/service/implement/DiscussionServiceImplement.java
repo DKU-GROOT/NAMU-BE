7eca3fac8dd4aaacacccfb65e5afb4b110fa4d6b
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-11 21:37:15 +0900

    discussion service implement

---
+++ namu/src/main/java/com/groot/namu/discussion/service/implement/DiscussionServiceImplement.java
@@ -0,0 +1,13 @@
+package com.groot.namu.discussion.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.groot.namu.discussion.service.DiscussionService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DiscussionServiceImplement implements DiscussionService{
+    
+}
