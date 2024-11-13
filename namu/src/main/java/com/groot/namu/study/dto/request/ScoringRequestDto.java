d840736ad4313262b34a9b6b73c65b0620a0f2d5
give-and-take <k5473029@naver.com>
2024-11-13 21:11:02 +0900

    compile error handling

--- namu/src/main/java/com/groot/namu/study/dto/request/ScoringRequestDto.java
+++ namu/src/main/java/com/groot/namu/study/dto/request/ScoringRequestDto.java
@@ -1,6 +1,5 @@
 package com.groot.namu.study.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
--- namu/src/main/java/com/groot/namu/study/dto/request/ShowMyStudyRequestDto.java
+++ namu/src/main/java/com/groot/namu/study/dto/request/ShowMyStudyRequestDto.java
@@ -1,13 +1,7 @@
 package com.groot.namu.study.dto.request;
 
-import com.groot.namu.study.common.StudyResponseCode;
-import com.groot.namu.study.common.StudyResponseMessage;
-import com.groot.namu.study.dto.response.ResponseDto;
-import com.groot.namu.study.dto.response.ShowMyStudyResponseDto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 @Getter
 public class ShowMyStudyRequestDto {
