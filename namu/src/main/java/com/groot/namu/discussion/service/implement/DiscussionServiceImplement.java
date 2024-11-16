8dddf2f789267e777d3d3f03e6f35d9b483a1bab
Lee Jae Hyung <nomagold57@gmail.com>
2024-11-17 07:41:26 +0900

    discussion service implement

--- namu/src/main/java/com/groot/namu/discussion/service/implement/DiscussionServiceImplement.java
+++ namu/src/main/java/com/groot/namu/discussion/service/implement/DiscussionServiceImplement.java
@@ -1,7 +1,10 @@
 package com.groot.namu.discussion.service.implement;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.groot.namu.discussion.dto.request.PostDiscussionRequestDto;
+import com.groot.namu.discussion.dto.response.PostDiscussionResponseDto;
 import com.groot.namu.discussion.service.DiscussionService;
 
 import lombok.RequiredArgsConstructor;
@@ -9,5 +12,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class DiscussionServiceImplement implements DiscussionService{
+
+    @Override
+    public ResponseEntity<? super PostDiscussionResponseDto> post(PostDiscussionRequestDto dto) {
+        
+        return PostDiscussionResponseDto.success();
+    }
     
 }
